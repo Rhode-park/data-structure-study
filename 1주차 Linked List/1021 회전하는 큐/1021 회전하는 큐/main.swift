13be93a1e2712d3da43b1e73f7b343916f1f74c2
Rhode-park <jinah.park.16@gmail.com>
2023-01-08 22:58:05 +0900

    week1 Linked List

---
+++ 1주차 Linked List/1021 회전하는 큐/1021 회전하는 큐/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  1021 회전하는 큐
+//
+//  Created by Jinah Park on 2023/01/08.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ 1주차 Linked List/1158 요세푸스 문제/1158 요세푸스 문제/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  1158 요세푸스 문제
+//
+//  Created by Jinah Park on 2023/01/08.
+//
+
+import Foundation
+
+print("Hello, World!")
+
